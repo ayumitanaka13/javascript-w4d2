6c54f52b025c657c88d781155a7c628985ffb573
ayumitanaka <tanakayumi13@gmail.com>
2021-02-03 09:04:06 -0800

    assignment

--- 00-dad-jokes/app.js
+++ 00-dad-jokes/app.js
@@ -1,16 +1,29 @@
-const ul = document.querySelector('ul');
 const btn = document.querySelector('button');
+// const ul = document.querySelector('ul');
 
-const getDataJokes = async () => {
-    const res = await axios.get('https://icanhazdadjoke.com', {
-        headers: { 'Accept': 'application/json' }
-    });
-    return res.data.joke;
-};
+// const getDataJokes = async () => {
+//     const res = await axios.get('https://icanhazdadjoke.com', {
+//         headers: { 'Accept': 'application/json' }
+//     });
+//     return res.data.joke;
+// };
+
+// btn.addEventListener('click', async function(e){
+//     e.preventDefault();
+//     const li = document.createElement('li');
+//     li.innerText =  await getDataJokes();
+//     ul.appendChild(li);
+// });
 
 btn.addEventListener('click', async function(e){
     e.preventDefault();
+
+    const option = { headers: { 'Accept': 'application/json' } };
+    const response = await axios.get('https://icanhazdadjoke.com', option);
+
     const li = document.createElement('li');
-    li.innerText =  await getDataJokes();
+    li.innerText =  response.data.joke;
+
+    const ul = document.querySelector('ul');
     ul.appendChild(li);
 });
